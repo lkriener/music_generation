4f51de2b1a7866807bffb777eac261ca80272223
Laura Kriener <laurakriener@gmail.com>
2019-04-05 23:13:57 +0200

    Translation from midi track to numpy array and back
    - functionality in src/midi_utils.py
    - example usage in Demo_midi_utils.ipynb

--- src/midi_utils.py
+++ src/midi_utils.py
@@ -39,19 +39,19 @@ def get_bpm(csv_string):
     bpm = 1./val * 1e6 * 60
     return bpm, default
 
-def get_ticks_per_quarter(track):
+def get_ticks_per_quarter(csv_string):
     # how many midi-timesteps are one quarter note
     # can be different for each track
     # therefore this function operates on a single track from the track_dict
     # info does not have to be included in track 
-    # in that case use midi-default (24)
+    # in that case use midi-default (1024)
     # return value, bool if default was used
     default = True
-    ticks = 24
-    for line in track:
+    ticks = 1024
+    for line in csv_string:
         # find line with Time_signature
         split_line = [x.strip() for x in line.split(',')]
-        if split_line[2] == 'Time_signature':
+        if split_line[2] == 'Header':
             ticks = int(split_line[5])
             default = False
             break
@@ -96,3 +96,108 @@ def reduce_to_single_track(filename, new_filename, tracknr):
         writer = midi.FileWriter(midiFile)
         writer.write(pattern)
     return
+
+def check_if_note(line):
+    # check if track-line is a note event
+    # second return value indicates if on or off event
+    split_line = [x.strip() for x in line.split(',')]
+    if split_line[2] == 'Note_on_c':
+        return True, 1
+    elif split_line[2] == 'Note_off_c':
+        return True, 0
+    else:
+        return False, -1
+
+def get_note_info(line):
+    # before calling, make sure that line really is note event
+    split_line = [x.strip() for x in line.split(',')]
+    pitch = int(split_line[4])
+    time = int(split_line[1])
+    return pitch, time
+    
+def write_track_line(track_nr, time, command, valuelist):
+    # write single line of midi track with given content
+    line = str(track_nr) + ', ' + str(time) + ', ' + str(command)
+    if len(valuelist) > 0:
+        for item in valuelist:
+            line += ', ' + str(item)
+    line += '\n'
+    return line
+    
+def midi_track_to_numpy(track):
+    # convert midi track to numpy array
+    # [[pitch0, duration0], [pitch1, duration1],...]
+    notes = []
+    for i, line in enumerate(track):
+        is_note, on_off = check_if_note(line)
+        if not is_note:
+            continue
+        else:
+            pitch, time = get_note_info(line)
+            notes.append([on_off, time, pitch])
+    # split into off- and on-events
+    mask = np.array([bool(elem[0]) for elem in notes])
+    on_notes = np.array(notes)[mask]
+    mask = np.logical_not(mask)
+    off_notes = np.array(notes)[mask]
+    result = []
+    for on_note in on_notes:
+        start = on_note[1]
+        pitch = on_note[2]
+        # find corresponding off-event
+        off_idx = np.where(off_notes[:,2] == pitch)
+        if len(off_idx[0]) == 0:
+            # no corresponding off-event found
+            # -> ignore on event
+            continue
+        off_idx = off_idx[0][0]
+        end = off_notes[off_idx][1]
+        result.append([pitch, end-start])
+        # delete off event (because off-event can only correspond to one on event)
+        off_notes = np.delete(off_notes, off_idx, axis=0)
+    return np.array(result)
+
+def numpy_to_midi_track(array, track_nr, title, key_signature=(0, 'major'), time_signature=[4, 2, 24, 8]):
+    # convert numpy array (as defined in midi_track to numpy) to midi track
+    # additional info about the track besides array must be supplied
+    track_list = []
+    track_list.append(write_track_line(track_nr, 0, 'Start_track', []))
+    track_list.append(write_track_line(track_nr, 0, 'Title_t', ['"'+title+'"']))
+    track_list.append(write_track_line(track_nr, 0, 'Key_signature', [key_signature[0], '"'+key_signature[1]+'"']))
+    track_list.append(write_track_line(track_nr, 0, 'Time_signature', time_signature))
+    time = 1024
+    for note in array:
+        track_list.append(write_track_line(track_nr, time, 'Note_on_c', [0, note[0], 90]))
+        time += note[1]
+        track_list.append(write_track_line(track_nr, time, 'Note_off_c', [0, note[0], 0]))
+    time += 1024
+    track_list.append(write_track_line(track_nr, 0, 'End_track', []))
+    return track_list
+    
+def write_to_midi(csv_list, new_filename):
+    # write csv_list to disc, convert csv file to midi file
+    with open(new_filename[:-4]+'.csv', 'w') as writeFile:
+        for line in csv_list:
+            writeFile.write(line)
+    # use provided function to load midi pattern from csv file
+    # in order to use built-in function to write midi file itself to disc
+    pattern = midi.csv_to_midi(new_filename[:-4]+'.csv')
+    with open(new_filename, 'wb') as midiFile:
+        writer = midi.FileWriter(midiFile)
+        writer.write(pattern)
+    return
+    
+def track_dict_to_csv(track_dict):
+    # convert track_dict to original csv_list
+    csv_list = []
+    for elem in track_dict['0'][:-1]:
+        csv_list.append(elem)
+    for key in track_dict.keys():
+        if key == '0':
+            continue
+        else:
+            for elem in track_dict[key]:
+                csv_list.append(elem)
+    csv_list.append(track_dict['0'][-1])
+    return csv_list
+            
