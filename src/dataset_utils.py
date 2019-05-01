5f515c4f3b99a1df958fead947d666bc2e716f0f
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2019-05-01 14:41:19 +0200

    Make first training pipeline work with midi output

--- src/dataset_utils.py
+++ src/dataset_utils.py
@@ -1,8 +1,16 @@
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+
 
 def get_dataset_representation_from_tracks(tracks, feature_qty=10, prediction_qty=1):
+    """
+    Get dataset in moving window representation from a list of tracks.
+    :param tracks:
+    :param feature_qty:
+    :param prediction_qty:
+    :return:
+    """
 
     x = []
     y = []
@@ -16,34 +24,45 @@ def get_dataset_representation_from_tracks(tracks, feature_qty=10, prediction_qt
 
 
 def get_dataset_representation_from_track(track, feature_qty=10, prediction_qty=1):
+    """
+    Get dataset in moving window representation from a single track.
+    :param track:
+    :param feature_qty:
+    :param prediction_qty:
+    :return:
+    """
 
     x = []
     y = []
     if len(track) < feature_qty + prediction_qty:
-        raise Exception("Track is shorter than {0} the number of features for data set representation.".format(feature_qty + prediction_qty))
+        raise Exception("Track is shorter than the moving window size {0} for dataset representation.".format(feature_qty + prediction_qty))
 
     sample_qty = len(track) - prediction_qty - feature_qty  # number of samples we can generate from one track if we always increment by one tone
 
     for first_note_index in range(sample_qty):
         features = track[first_note_index:first_note_index + feature_qty]
         prediction = track[first_note_index + feature_qty: first_note_index + feature_qty + prediction_qty]
+   
         x.append(features)
         y.append(prediction)
-
+        
     return np.array(x), np.array(y)
 
 
 class TrackDataset(Dataset):
-    def __init__(self, x, y):
+    def __init__(self, x, y, drop_length=False):
         super(TrackDataset, self).__init__()
+
         self.x = x
         self.y = y
+        self.drop_length = drop_length
 
     def __len__(self):
         """
         Denotes the total number of samples.
         :return:
         """
+
         return len(self.x)
 
     def __getitem__(self, index):
@@ -54,10 +73,12 @@ class TrackDataset(Dataset):
 
         # Select sample
         x = self.x[index]
-        x = x[:, 0] # take only the pitch
+        if self.drop_length:
+            x = x[:, 0]
         x = torch.tensor(x, dtype=torch.float32)
 
         y = self.y[index]
-        y = y[:, 0] # take only the pitch
+        if self.drop_length:
+            y = y[:, 0]
         y = torch.tensor(y, dtype=torch.float32)
         return x, y
--- src/midi_utils.py
+++ src/midi_utils.py
@@ -5,15 +5,28 @@ import py_midicsv as midi
 
 
 def load_to_csv(filepath):
+    """
+    Load midi to csv.
+
+    :param filepath:
+    :return:
+    """
     s = midi.midi_to_csv(filepath)
     return s
 
 
 def split_tracks(csv_string):
-    # first number in each line is the track number
-    # track 0 always contains some additional info, not the music
-    # returns a dictionary with track numbers as keys 
-    # and list of csv lines as values
+    """
+    Split song into tracks.
+
+    first number in each line is the track number
+    track 0 always contains some additional info, not the music
+    returns a dictionary with track numbers as keys
+    and list of csv lines as values
+    :param csv_string:
+    :return:
+    """
+
     tracks = {}
     for line in csv_string:
         channel = line[0]
@@ -25,10 +38,17 @@ def split_tracks(csv_string):
 
 
 def get_bpm(csv_string):
-    # assume no tempo changes in file
-    # necessary value does not have to be included in file
-    # in that case use midi-default
-    # return value, bool if default was used
+    """
+    Get beats per minute of the song.
+
+    assume no tempo changes in file
+    necessary value does not have to be included in file
+    in that case use midi-default
+    return value, bool if default was used
+    :param csv_string:
+    :return:
+    """
+
     val = -1
     default = True
     for line in csv_string:
@@ -38,6 +58,7 @@ def get_bpm(csv_string):
             val = int(split_line[3])
             default = False
             break
+
     # midi default tempo: 500000 microseconds per quarter note
     if default:
         val = 500000
@@ -46,12 +67,18 @@ def get_bpm(csv_string):
 
 
 def get_ticks_per_quarter(csv_string):
-    # how many midi-timesteps are one quarter note
-    # can be different for each track
-    # therefore this function operates on a single track from the track_dict
-    # info does not have to be included in track 
-    # in that case use midi-default (1024)
-    # return value, bool if default was used
+    """
+    Get the number of midi-timesteps (ticks) per quarter note.
+
+    can be different for each track
+    therefore this function operates on a single track from the track_dict
+    info does not have to be included in track
+    in that case use midi-default (1024)
+    return value, bool if default was used
+    :param csv_string:
+    :return:
+    """
+
     default = True
     ticks = 1024
     for line in csv_string:
@@ -65,7 +92,15 @@ def get_ticks_per_quarter(csv_string):
 
 
 def replace_int_in_line(line, pos, new_val):
-    # replace an int-value in a csv line at pos with a new value
+    """
+    Replace an int-value in a csv line at pos with a new value.
+
+    :param line:
+    :param pos:
+    :param new_val:
+    :return:
+    """
+
     split_line = [x.strip() for x in line.split(',')]
     split_line[pos] = str(new_val)
     new_line = ''
@@ -77,26 +112,40 @@ def replace_int_in_line(line, pos, new_val):
     return new_line
 
 
-def reduce_to_single_track(filename, new_filename, tracknr):
-    # can be used to make file monophonic if the tracknr of the track 
-    # that should be kept is known (has to be determined with other functions)
-    # takes filename of polyphonic mide and name of file to which monophonic should be written
+def reduce_to_single_track(filename, new_filename, track_nr):
+    """
+    Reduce a file to a single track and save it to a new file.
+
+    can be used to make file monophonic if the track nr of the track
+    that should be kept is known (has to be determined with other functions)
+    takes filename of polyphonic midi and name of file to which monophonic should be written
+    :param filename:
+    :param new_filename:
+    :param track_nr:
+    :return:
+    """
+
     s = load_to_csv(filename)
     track_dict = split_tracks(s)
     new_csv = []
+
     # must keep first and last line of track '0'
     header_line = track_dict['0'][0]
+
     # change the number of tracks that is written there to 1
     header_line = replace_int_in_line(header_line, 4, 1)
     new_csv.append(header_line)
+
     # copy lines of chosen track to new csv
-    for elem in track_dict[tracknr]:
+    for elem in track_dict[track_nr]:
         new_csv.append(elem)
     new_csv.append(track_dict['0'][-1])
+
     # write new csv to disc
     with open(new_filename[:-4] + '.csv', 'w') as writeFile:
         for line in new_csv:
             writeFile.write(line)
+
     # use provided function to load midi pattern from csv file
     # in order to use built-in function to write midi file itself to disc
     pattern = midi.csv_to_midi(new_filename[:-4] + '.csv')
@@ -107,8 +156,14 @@ def reduce_to_single_track(filename, new_filename, tracknr):
 
 
 def check_if_note(line):
-    # check if track-line is a note event
-    # second return value indicates if on or off event
+    """
+    Check if track-line is a note event.
+
+    second return value indicates if on or off event
+    :param line:
+    :return:
+    """
+
     split_line = [x.strip() for x in line.split(',')]
     if split_line[2] == 'Note_on_c':
         return True, 1
@@ -119,26 +174,50 @@ def check_if_note(line):
 
 
 def get_note_info(line):
+    """
+    Get note pitch and length.
+
+    :param line:
+    :return:
+    """
     # before calling, make sure that line really is note event
+    if not check_if_note(line)[0]:
+        raise Exception("Provided line is not a note event!")
+
     split_line = [x.strip() for x in line.split(',')]
     pitch = int(split_line[4])
     time = int(split_line[1])
     return pitch, time
 
 
-def write_track_line(track_nr, time, command, valuelist):
-    # write single line of midi track with given content
+def _write_track_line(track_nr, time, command, value_list):
+    """
+    Write single line of midi track with given content.
+
+    :param track_nr:
+    :param time:
+    :param command:
+    :param value_list:
+    :return:
+    """
+
     line = str(track_nr) + ', ' + str(time) + ', ' + str(command)
-    if len(valuelist) > 0:
-        for item in valuelist:
+    if len(value_list) > 0:
+        for item in value_list:
             line += ', ' + str(item)
     line += '\n'
     return line
 
 
 def midi_track_to_numpy(track):
-    # convert midi track to numpy array
-    # [[pitch0, duration0], [pitch1, duration1],...]
+    """
+    Convert midi track to numpy array.
+
+    Format: [[pitch0, duration0], [pitch1, duration1],...]
+    :param track:
+    :return:
+    """
+
     notes = []
     for i, line in enumerate(track):
         is_note, on_off = check_if_note(line)
@@ -162,7 +241,7 @@ def midi_track_to_numpy(track):
 
         # if there is a gap between the previous note and the current one
         # TODO: If we want to model silence, consider adding this again
-        #if previous_end != -1 and previous_end < start:
+        # if previous_end != -1 and previous_end < start:
         #    result.append([-1, start - previous_end])  # add silence as a pitch
 
         # find corresponding off-event
@@ -182,25 +261,50 @@ def midi_track_to_numpy(track):
 
 
 def numpy_to_midi_track(array, track_nr, title, key_signature=(0, 'major'), time_signature=[4, 2, 24, 8]):
-    # convert numpy array (as defined in midi_track to numpy) to midi track
-    # additional info about the track besides array must be supplied
+    """
+    Convert numpy array (as defined in midi_track to numpy) to midi track.
+
+    additional info about the track besides array must be supplied
+    :param array:
+    :param track_nr:
+    :param title:
+    :param key_signature:
+    :param time_signature:
+    :return:
+    """
+
     track_list = []
-    track_list.append(write_track_line(track_nr, 0, 'Start_track', []))
-    track_list.append(write_track_line(track_nr, 0, 'Title_t', ['"' + title + '"']))
-    track_list.append(write_track_line(track_nr, 0, 'Key_signature', [key_signature[0], '"' + key_signature[1] + '"']))
-    track_list.append(write_track_line(track_nr, 0, 'Time_signature', time_signature))
+    track_list.append(_write_track_line(track_nr, 0, 'Start_track', []))
+    track_list.append(_write_track_line(track_nr, 0, 'Title_t', ['"' + title + '"']))
+    track_list.append(_write_track_line(track_nr, 0, 'Key_signature', [key_signature[0], '"' + key_signature[1] + '"']))
+    track_list.append(_write_track_line(track_nr, 0, 'Time_signature', time_signature))
     time = 1024
     for note in array:
-        track_list.append(write_track_line(track_nr, time, 'Note_on_c', [0, note[0], 90]))
+        track_list.append(_write_track_line(track_nr, int(time), 'Note_on_c', [0, int(note[0]), 90]))
         time += note[1]
-        track_list.append(write_track_line(track_nr, time, 'Note_off_c', [0, note[0], 0]))
+        track_list.append(_write_track_line(track_nr, int(time), 'Note_off_c', [0, int(note[0]), 0]))
     time += 1024
-    track_list.append(write_track_line(track_nr, 0, 'End_track', []))
+    track_list.append(_write_track_line(track_nr, 0, 'End_track', []))
     return track_list
 
+def prediction_to_numpy(predictions, default_note_length=1024):
+    numpy_list = []
+    if len(predictions.shape) == 1: # pitch prediction only -> we need to add the default length
+        for x in np.nditer(predictions):
+            numpy_list.append([x, 1024])
+            
+    return np.array(numpy_list)
+        
 
 def write_to_midi(csv_list, new_filename):
-    # write csv_list to disc, convert csv file to midi file
+    """
+    Write csv_list to disc, convert csv file to midi file.
+
+    :param csv_list:
+    :param new_filename:
+    :return:
+    """
+
     with open(new_filename[:-4] + '.csv', 'w') as writeFile:
         for line in csv_list:
             writeFile.write(line)
@@ -214,7 +318,13 @@ def write_to_midi(csv_list, new_filename):
 
 
 def track_dict_to_csv(track_dict):
-    # convert track_dict to original csv_list
+    """
+    Convert track_dict to original csv_list.
+
+    :param track_dict:
+    :return:
+    """
+
     csv_list = []
     for elem in track_dict['0'][:-1]:
         csv_list.append(elem)
@@ -229,13 +339,24 @@ def track_dict_to_csv(track_dict):
 
 
 def find_lead_track(track_dict, random=True, largest_range=False, variation=False, rhythm=False):
-    # determine the lead track using chosen criterions
-    # Remove Header track because it is not really a track which contains music
+    """
+    Determine the lead track using chosen criteria.
+
+    Remove Header track because it is not really a track which contains music
+    :param track_dict:
+    :param random:
+    :param largest_range:
+    :param variation:
+    :param rhythm:
+    :return:
+    """
+
     new_track_dict = copy.deepcopy(track_dict)
     del new_track_dict['0']
     key_list = list(new_track_dict.keys())
     print('track_nrs:', key_list)
     num_tracks = len(key_list)
+
     # if criterion random is active all others are ignored
     if random:
         nr = r.randint(0, num_tracks - 1)
@@ -245,6 +366,7 @@ def find_lead_track(track_dict, random=True, largest_range=False, variation=Fals
     for key in key_list:
         numpy_track = midi_track_to_numpy(new_track_dict[key])
         numpy_tracks.append(numpy_track)
+
     # largest_range choses the track with highes difference between lowest and highest note
     if largest_range:
         ranges = []
@@ -255,6 +377,7 @@ def find_lead_track(track_dict, random=True, largest_range=False, variation=Fals
         track_idx = np.argmax(ranges)
         scores[track_idx] += 1
         print('Note range:', ranges)
+
     # find the track with the most different notes
     if variation:
         num_diff_notes = []
@@ -264,6 +387,7 @@ def find_lead_track(track_dict, random=True, largest_range=False, variation=Fals
         track_idx = np.argmax(num_diff_notes)
         print('Number of different notes:', num_diff_notes)
         scores[track_idx] += 1
+
     # find the track with the most different note lengths indicating most rhythm
     if rhythm:
         num_diff_lengths = []
@@ -284,13 +408,26 @@ def find_lead_track(track_dict, random=True, largest_range=False, variation=Fals
 
 
 def get_mean_pitch(numpy_track):
-    # return the mean pitch (rounded to integer, i.e. real note)
+    """
+    Return the mean pitch (rounded to integer, i.e. real note).
+
+    :param numpy_track:
+    :return:
+    """
+
     mean_pitch = np.mean(numpy_track, axis=0)[0]
     return np.around(mean_pitch)
 
 
 def shift_track_pitch(numpy_track, delta_pitch):
-    # shift the pitch of the notes in the track by given delta
+    """
+    Shift the pitch of the notes in the track by given delta.
+
+    :param numpy_track:
+    :param delta_pitch:
+    :return:
+    """
+
     mask = np.reshape([1, 0] * len(numpy_track), (len(numpy_track), 2)) * delta_pitch
     new_track = numpy_track + mask
     return new_track
