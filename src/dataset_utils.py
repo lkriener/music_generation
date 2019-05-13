1b154e732058505c58e8256dcec7bd818c79db44
Laura Kriener <laurakriener@gmail.com>
2019-05-13 23:04:45 +0200

    extend dataset_utils.py for intervals

--- src/dataset_utils.py
+++ src/dataset_utils.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils.data import Dataset
 
 
-def get_dataset_representation_from_tracks(tracks, feature_qty=10, prediction_qty=1):
+def get_dataset_representation_from_tracks(tracks, feature_qty=10, prediction_qty=1, intervals=False):
     """
     Get dataset in moving window representation from a list of tracks.
     :param tracks:
@@ -16,7 +16,10 @@ def get_dataset_representation_from_tracks(tracks, feature_qty=10, prediction_qt
     y = []
 
     for track in tracks:
-        track_x, track_y = get_dataset_representation_from_track(track, feature_qty, prediction_qty)
+        if intervals:
+            track_x, track_y = get_dataset_representation_from_track_intervals(track, feature_qty, prediction_qty)
+        else:
+            track_x, track_y = get_dataset_representation_from_track(track, feature_qty, prediction_qty)
         x.extend(track_x)
         y.extend(track_y)
 
@@ -49,12 +52,48 @@ def get_dataset_representation_from_track(track, feature_qty=10, prediction_qty=
     return np.array(x), np.array(y)
 
 
+def get_dataset_representation_from_track_intervals(track, feature_qty=10, prediction_qty=1):
+    """
+    Get dataset in moving window representation from a single track in interval formulation
+    :param track:
+    :param feature_qty:
+    :param prediction_qty:
+    :return:
+    """
+
+    x = []
+    y = []
+    if len(track) < feature_qty + prediction_qty:
+        raise Exception("Track is shorter than the moving window size {0} for dataset representation.".format(feature_qty + prediction_qty))
+
+    sample_qty = len(track) - prediction_qty - feature_qty - 2  # number of samples we can generate from one track if we always increment by one tone
+
+    for first_note_index in range(sample_qty):
+        # features
+        pitches = track[first_note_index:first_note_index + feature_qty + 1][:,0]
+        lengths = track[first_note_index:first_note_index + feature_qty + 1][:,1]
+        intervals = np.diff(pitches)
+        features = np.dstack((intervals, lengths[:-1]))[0]
+        # prediction
+        pitches = track[first_note_index + feature_qty: first_note_index + feature_qty + prediction_qty + 1][:,0]
+        lengths = track[first_note_index + feature_qty: first_note_index + feature_qty + prediction_qty + 1][:,1]
+        intervals = np.diff(pitches)
+        prediction = np.dstack((intervals, lengths[:-1]))[0]
+   
+        x.append(features)
+        y.append(prediction)
+        
+    return np.array(x), np.array(y)
+
+
+
 class TrackDataset(Dataset):
-    def __init__(self, x, y, drop_length=False):
+    def __init__(self, x, y, drop_length=False, intervals=False):
         super(TrackDataset, self).__init__()
 
         self.x = x
         self.y = y
+        self.intervals = intervals
         self.drop_length = drop_length
 
     def __len__(self):
@@ -80,5 +119,8 @@ class TrackDataset(Dataset):
         y = self.y[index]
         if self.drop_length:
             y = y[:, 0]
-        y = torch.tensor(y, dtype=torch.float32)
+        if self.intervals:
+            y = torch.tensor(y, dtype=torch.long)
+        else:
+            y = torch.tensor(y, dtype=torch.float32)
         return x, y
