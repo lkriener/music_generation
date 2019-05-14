306d75b484aaf7a30553a46f80f843a4c14148b1
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2019-05-14 13:54:47 +0200

    Load and preprocess midi songs into a representation similar to pianoroll

--- src/midi_utils.py
+++ src/midi_utils.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import copy
 import random as r
 import numpy as np
 import py_midicsv as midi
+from mido import MidiFile, MidiTrack, Message
 
 
 def load_to_csv(filepath):
@@ -312,9 +316,10 @@ def numpy_to_midi_track(array, track_nr, title, key_signature=(0, 'major'), time
     track_list.append(_write_track_line(track_nr, 0, 'End_track', []))
     return track_list
 
+
 def prediction_to_numpy(predictions, default_note_length=1024):
     numpy_list = []
-    if len(predictions.shape) == 1: # pitch prediction only -> we need to add the default length
+    if len(predictions.shape) == 1:  # pitch prediction only -> we need to add the default length
         for x in np.nditer(predictions):
             numpy_list.append([x, 1024])
             
@@ -469,8 +474,6 @@ def translate_numpy_pianoroll(numpy_track, ticks_per_16):
     return np.array(pianoroll)
 
 
-
-
 def one_hot_encode_batch(flattened_pianoroll, n_notes):
     """
     Return a one-hot batch to perform RNN training
@@ -501,10 +504,9 @@ def one_hot_encode_pianoroll(flattened_pianoroll, n_notes):
     """
     one_hot = np.zeros((len(flattened_pianoroll), n_notes))
     for i in range(len(flattened_pianoroll)):
-        if flattened_pianoroll[i] > 0: # if it is a note, and not a silence
-            one_hot[i,flattened_pianoroll[i]] = 1
+        if flattened_pianoroll[i] > 0:  # if it is a note, and not a silence
+            one_hot[i, flattened_pianoroll[i]] = 1
     return one_hot
-
 
 
 def flatten_one_hot_pianoroll(one_hot_pianoroll):
@@ -515,7 +517,8 @@ def flatten_one_hot_pianoroll(one_hot_pianoroll):
     """
     flattened_pianoroll = np.argmax(one_hot_pianoroll, axis=1)
     return flattened_pianoroll
-    
+
+
 def scale_pianoroll(flattened_pianoroll, global_lower):
     """
     Scales flattened pianoroll to values near 0
@@ -528,6 +531,7 @@ def scale_pianoroll(flattened_pianoroll, global_lower):
     scaled_pianoroll[np.where(scaled_pianoroll<0)] = 0 
     return scaled_pianoroll
 
+
 def unscale_pianoroll(scaled_pianoroll, global_lower):
     """
     Returns the pianoroll in the initial config.
@@ -537,21 +541,20 @@ def unscale_pianoroll(scaled_pianoroll, global_lower):
     :return:
     """
     unscaled_pianoroll = np.copy(scaled_pianoroll + global_lower - 1)
-    unscaled_pianoroll[np.where(unscaled_pianoroll == global_lower-1)] = 0 # reset the silences to 0
+    unscaled_pianoroll[np.where(unscaled_pianoroll == global_lower-1)] = 0  # reset the silences to 0
     return unscaled_pianoroll 
-
 
 
 # Defining method to make mini-batches for training
 def get_pianoroll_batches(arr, batch_size, seq_length):
-    '''
+    """
     Create a generator that returns batches of size
     batch_size x seq_length from arr.
        
     :arr: Array you want to make batches from
     :batch_size: Batch size, the number of sequences per batch
     :seq_length: Number of encoded notes in a sequence
-    '''
+    """
     
     batch_size_total = batch_size * seq_length
     # total number of batches we can make
@@ -574,17 +577,16 @@ def get_pianoroll_batches(arr, batch_size, seq_length):
             y[:, :-1], y[:, -1] = x[:, 1:], arr[:, 0]
         yield x, y
 
-        
 
 def get_pianoroll_batches_harmonization(arr, arr2, batch_size, seq_length):
-    '''
+    """
     Create a generator that returns batches of size
     batch_size x seq_length from arr.
        
     :arr: Array you want to make batches from
     :batch_size: Batch size, the number of sequences per batch
     :seq_length: Number of encoded notes in a sequence
-    '''
+    """
     
     batch_size_total = batch_size * seq_length
     # total number of batches we can make
@@ -606,8 +608,154 @@ def get_pianoroll_batches_harmonization(arr, arr2, batch_size, seq_length):
         yield x, y
 
 
+def midi_to_samples(file_name, encode_length=False, num_notes=96, samples_per_measure=96):
+    """
+    Turn a midi file into a sample.
+    :param file_name:
+    :param encode_length:
+    :param num_notes:
+    :param samples_per_measure:
+    :return:
+    """
+    has_time_sig = False
+    mid = MidiFile(file_name)
 
-    
+    ticks_per_beat = mid.ticks_per_beat  # get ticks per beat
+    ticks_per_measure = 4 * ticks_per_beat  # get ticks per measure
+
+    # detect the time signature of the midi
+    for track in mid.tracks:
+        for msg in track:
+            if msg.type == 'time_signature':
+                new_tpm = ticks_per_measure * msg.numerator / msg.denominator  # adapt ticks per measure of this specific song
+
+                # skip if we find multiple time signatures in the song
+                if has_time_sig and new_tpm != ticks_per_measure:
+                    raise NotImplementedError('Multiple time signatures not supported')
+
+                ticks_per_measure = new_tpm
+                has_time_sig = True
+
+    # turn tracks into pianoroll representation
+    all_notes = {}
+    for track in mid.tracks:
+
+        abs_time = 0
+        for msg in track:
+            abs_time += msg.time  # step time forward
+
+            # we skip programs 0x70-0x7F which are percussion and sound effects
+            if msg.type == 'program_change' and msg.program >= 0x70:
+                break
+
+            # if a note starts
+            if msg.type == 'note_on':
+
+                # we skip notes without a velocity (basically how strong a note is played to make it sound human)
+                if msg.velocity == 0:
+                    continue
+
+                # transform the notes into the 96 heights
+                note = msg.note - (128 - num_notes) / 2
+                if note < 0 or note >= num_notes:  # ignore a note that is outside of that range
+                    print('Ignoring', file_name, 'note is outside 0-%d range' % (num_notes - 1))
+                    return []
+
+                # count the number of played notes per pitch
+                if note not in all_notes:
+                    all_notes[note] = []
+                else:
+                    single_note = all_notes[note][-1]
+                    if len(single_note) == 1:
+                        single_note.append(single_note[0] + 1)
+
+                # store the time a note has been played
+                all_notes[note].append([abs_time * samples_per_measure / ticks_per_measure])
+
+            # if a note ends
+            elif msg.type == 'note_off':
+
+                # if the note has already ended before (note_on, note_off, note_off), we skip the event
+                if len(all_notes[note][-1]) != 1:
+                    continue
+                # store the time a note stops playing
+                all_notes[note][-1].append(abs_time * samples_per_measure / ticks_per_measure)
+
+    # any note did not end playing, we end it one time tick later
+    for note in all_notes:
+        for start_end in all_notes[note]:
+            if len(start_end) == 1:
+                start_end.append(start_end[0] + 1)
+
+    # put the notes into their respective sample/measure panel (96 x 96)
+    samples = []
+    for note in all_notes:
+        for start, end in all_notes[note]:
+            sample_ix = int(start / samples_per_measure)  # find the sample/measure this belongs into
+            assert (sample_ix < 1024 * 1024)
+
+            # fill in silence until the appropriate sample/measure is reached
+            while len(samples) <= sample_ix:
+                samples.append(np.zeros((samples_per_measure, num_notes), dtype=np.uint8))
+
+            # get sample and find its start to encode the start of the note
+            sample = samples[sample_ix]
+            start_ix = int(start - sample_ix * samples_per_measure)
+            sample[start_ix, int(note)] = 1
+
+            # play note until it ends if we encode length
+            if encode_length:
+                end_ix = min(end - sample_ix * samples_per_measure, samples_per_measure)
+                while start_ix < end_ix:
+                    sample[start_ix, int(note)] = 1
+                    start_ix += 1
+
+    return samples
 
 
-    
+def samples_to_midi(samples, file_name, threshold=0.5, num_notes=96, samples_per_measure=96):
+    """
+    Turn the samples/measures back into midi.
+    :param samples:
+    :param file_name:
+    :param threshold:
+    :param num_notes:
+    :param samples_per_measure:
+    :return:
+    """
+    # TODO: Encode the certainties of the notes into the volume of the midi for the notes that are above threshold
+
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
+
+    ticks_per_beat = mid.ticks_per_beat
+    ticks_per_measure = 4 * ticks_per_beat
+    ticks_per_sample = ticks_per_measure / samples_per_measure
+
+    # add instrument for track
+    # https://en.wikipedia.org/wiki/General_MIDI#Program_change_events
+    piano = 1
+    honky_tonk_piano = 4
+    xylophone = 14
+    program_message = Message('program_change', program=piano, time=0, channel=0)
+    track.append(program_message)
+
+    abs_time = 0
+    last_time = 0
+    for sample in samples:
+        for y in range(sample.shape[0]):
+            abs_time += ticks_per_sample
+            for x in range(sample.shape[1]):
+                note = x + (128 - num_notes) / 2
+
+                if sample[y, x] >= threshold and (y == 0 or sample[y - 1, x] < threshold):
+                    delta_time = abs_time - last_time
+                    track.append(Message('note_on', note=int(note), velocity=127, time=int(delta_time)))
+                    last_time = abs_time
+
+                if sample[y, x] >= threshold and (y == sample.shape[0] - 1 or sample[y + 1, x] < threshold):
+                    delta_time = abs_time - last_time
+                    track.append(Message('note_off', note=int(note), velocity=127, time=int(delta_time)))
+                    last_time = abs_time
+    mid.save(file_name)
---
+++ src/preprocess_songs.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Load songs from midi, preprocess and save them in numpy format.
+"""
+
+import os  # file system operations
+import numpy as np  #
+import argparse
+
+import src.midi_utils as midi_utils
+import src.music_utils as music_utils
+
+
+def preprocess_songs(data_folders):
+    """
+    Load and preprocess the songs from the data folders and turn them into a dataset of samples/pitches and lengths of the tones.
+    :param data_folders:
+    :return:
+    """
+
+    all_samples = []
+    all_lengths = []
+
+    # keep some statistics
+    succeeded = 0
+    failed = 0
+    ignored = 0
+
+    # load songs
+    print("Loading songs...")
+    # walk folders and look for midi files
+    for folder in data_folders:
+        for root, _, files in os.walk(folder):
+            for file in files:
+                path = os.path.join(root, file)
+                if not (path.endswith('.mid') or path.endswith('.midi')):
+                    continue
+
+                # turn midi into samples
+                try:
+                    samples = midi_utils.midi_to_samples(path)
+                except Exception as e:
+                    print("ERROR ", path)
+                    print(e)
+                    failed += 1
+                    continue
+
+                # if the midi does not produce the minimal number of sample/measures, we skip it
+                if len(samples) < 16:
+                    print('WARN', path, 'Sample too short, unused')
+                    ignored += 1
+                    continue
+
+                # transpose samples (center them in full range to get more training samples for the same tones)
+                samples, lengths = music_utils.generate_centered_transpose(samples)
+                all_samples += samples
+                all_lengths += lengths
+                print('SUCCESS', path, len(samples), 'samples')
+                succeeded += 1
+
+    assert (sum(all_lengths) == len(all_samples))  # assert equal number of samples and lengths
+
+    # save all to disk
+    print("Saving " + str(len(all_samples)) + " samples...")
+    all_samples = np.array(all_samples, dtype=np.uint8)  # reduce size when saving
+    all_lengths = np.array(all_lengths, dtype=np.uint32)
+    np.save('../data/interim/samples.npy', all_samples)
+    np.save('../data/interim/lengths.npy', all_lengths)
+    print('Done: ', succeeded, 'succeded,', ignored, 'ignored,', failed, 'failed of', succeeded + ignored + failed, 'in total')
+
+
+if __name__ == "__main__":
+    # configure parser and parse arguments
+    parser = argparse.ArgumentParser(description='Load songs, preprocess them and put them into a dataset.')
+    parser.add_argument('--data_folder', default=["../data/raw/bach"], type=str, help='The path to the midi data', action='append')
+
+    args = parser.parse_args()
+    preprocess_songs(args.data_folder)
